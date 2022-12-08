6dc91e5e23d8d841bc4a691d41ab7f8027aa0457
denis.topallaj <denis.topallaj13@gmail.com>
2022-12-08 13:18:56 +0100

    added a few tests

--- src/test/java/be/howest/ti/mars/logic/data/MarsH2RepositoryTest.java
+++ src/test/java/be/howest/ti/mars/logic/data/MarsH2RepositoryTest.java
@@ -1,12 +1,19 @@
 package be.howest.ti.mars.logic.data;
 
+import be.howest.ti.mars.logic.domain.Incident;
 import be.howest.ti.mars.logic.domain.User;
+import be.howest.ti.mars.logic.exceptions.RepositoryException;
 import io.vertx.core.json.JsonObject;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
 
 class MarsH2RepositoryTest {
     private static final String URL = "jdbc:h2:./db-11";
@@ -22,16 +29,75 @@ class MarsH2RepositoryTest {
     }
 
     @Test
-    void dummyTestForSonar(){
+    void getIncidents(){
         // Arrange
-        int id = 1;
-
+        int lengthOfIncidentsList = 7;
         // Act
-        User user = new User("1989-01-28_AL", "Adison", "Lopez", false);
-
+        List<Incident> incidentList = Repositories.getH2Repo().getIncidents();
         //Assert
-        Assertions.assertEquals("Adison", user.getFirstname());
+        Assertions.assertEquals(lengthOfIncidentsList, incidentList.size());
     }
+
+    @Test
+    void getLabelsFromIncidents(){
+        // Arrange
+        int incidentId = 1;
+        List<String> listOfLabels = List.of("Armed", "Child Danger", "Critical Condition");
+        // Act
+        Incident incidentFromDB = Repositories.getH2Repo().getIncidents().stream().filter(incident -> incident.getId() == incidentId).findAny().orElseThrow(() -> new NoSuchElementException("Incident with this id doesn't exist"));
+        ;
+        //Assert
+        Assertions.assertEquals(listOfLabels, incidentFromDB.getLabels());
+    }
+
+    @Test
+    void getExceptionFromOneIncident(){
+        // Arrange
+        int incidentId = -1;
+
+        //Act + Assert
+        Assertions.assertThrows(NoSuchElementException.class, () -> Repositories.getH2Repo().getIncidents().stream().filter(incident -> incident.getId() == incidentId).findAny().orElseThrow(() -> new NoSuchElementException("Incident with this id doesn't exist")));
+    }
+
+    @Test
+    void getUser(){
+        //Arrange
+        String userId = "1989-01-28_AL";
+        String firstname = "Adison";
+        String lastname = "Lopez";
+        boolean subscribed = false;
+        //Act
+        User user = Repositories.getH2Repo().getUser(userId);
+        //Assert
+        Assertions.assertEquals(firstname, user.getFirstname());
+        Assertions.assertEquals(lastname, user.getLastname());
+        Assertions.assertEquals(subscribed, user.isSubscribed());
+    }
+
+    @Test
+    void subscribeUser(){
+        //Arrange
+        String userId = "1989-01-28_AL";
+        boolean subscribed = true;
+        //Act
+        User user = Repositories.getH2Repo().subscribeUser(userId);
+        //Assert
+        Assertions.assertEquals(subscribed, user.isSubscribed());
+
+    }
+
+    @Test
+    void unSubscribeUser(){
+        //Arrange
+        String userId = "1989-01-28_AL";
+        boolean subscribed = false;
+        //Act
+        User user = Repositories.getH2Repo().unSubscribeUser(userId);
+        //Assert
+        Assertions.assertEquals(subscribed, user.isSubscribed());
+
+    }
+
 /* STILL NEEDED, NO TESTS MADE YET USING OUR VERSION
     @Test
     void getQuote() {
